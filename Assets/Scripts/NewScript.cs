a70e10854f7d509c04a728f03a7205f0f0381d43
simen wiik <simenwiik@hotmail.com>
2021-09-26 14:06:14 +0200

    Adds URP Assets and testing state persistence

---
+++ Assets/Scripts/NewScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    [Range(1, 500)]
+    public int SomeInt;
+
+    public GameObject myObj;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

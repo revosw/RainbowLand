68aef5934f4a79870f6a0df5061c6e5b88e85953
Mathias van der Bend <mathiasvdbend@hotmail.com>
2021-10-22 10:52:20 +0200

    Added health pickup, health system, and a Beer enemy that damages the player on touch.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public Rigidbody2D rigidBody2D;
+    public float moveSpeed, jumpForce;
+    private bool isGrounded;
+    private float inputX;
+    const float groundCheckRadius = 0.2f;
+    public Transform groundCheckCollider;
+    public LayerMask groundLayer;
+
+    bool airjump = false;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    bool GroundCheck() {
+        isGrounded = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundLayer);
+        if (colliders.Length > 0) {
+            isGrounded = true;
+        }
+        return isGrounded;
+    }
+
     // Update is called once per frame
     void Update()
     {
-       
+        isGrounded = Physics2D.OverlapCircle(groundCheckCollider.position, groundCheckRadius, groundLayer);
+        rigidBody2D.velocity = new Vector2(inputX * moveSpeed, rigidBody2D.velocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context) {
+        inputX = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context) {
+        Debug.Log(isGrounded);
+        if (context.performed && GroundCheck()) {
+            rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpForce);
+            airjump = true;
+        } else if(airjump == true && context.performed) {
+            rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpForce);
+            airjump = false;
+        }
+        
     }
 }
---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+
+    private void Awake() {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(float _damage) {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+        if (currentHealth > 0) {
+            // take damage animation
+        } else {
+            // DEAD
+            GetComponent<PlayerMovement>().enabled = false;
+
+        }
+    }
+
+    public void Heal(float _heal) {
+        if (currentHealth < 3) {
+            currentHealth += _heal;
+        }
+    }
+
+    private void Update() {
+
+    }
+
+}

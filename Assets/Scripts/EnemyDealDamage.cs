aae9f0f98eae79a356f477e5718394402ae48ed5
Mathias van der Bend <mathiasvdbend@hotmail.com>
2021-11-19 18:13:44 +0100

    ?

--- Assets/Scripts/EnemyDealDamage.cs
+++ Assets/Scripts/EnemyDealDamage.cs
@@ -8,7 +8,6 @@ public class EnemyDealDamage : MonoBehaviour
     [SerializeField] private float damage;
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        
         if (collision.tag == "player") {
             collision.GetComponent<Health>().TakeDamage(damage);
         } else {

9bee1658125724e623a5b3e0bb8077ae4171b6a4
olav-valle <olavvalle@gmail.com>
2021-11-19 11:03:16 +0100

    health, damage, checkpoint and respawn works

--- Assets/Scripts/Checkpoint/GameMaster.cs
+++ Assets/Scripts/Checkpoint/GameMaster.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class GameMaster : MonoBehaviour {
     private static GameMaster instance;
     public Vector2 lastCheckPointPosition;
+    private Transform player;
 
     private void Awake() {
         if (instance == null) {
             instance = this;
+            player = GameObject.FindGameObjectWithTag("player").GetComponent<Transform>();
+            // set checkpoint position to player starting position.
+            lastCheckPointPosition = player.position;
+            
             DontDestroyOnLoad(instance);
         } else {
             Destroy(gameObject);

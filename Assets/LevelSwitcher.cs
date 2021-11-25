e950ecee3214c868aa909ce8f12819629bada70a
Simen Wiik <simenwiik@hotmail.com>
2021-11-26 00:54:31 +0100

    adds banner and fade to black between levels

--- Assets/LevelSwitcher.cs
+++ Assets/LevelSwitcher.cs
@@ -2,25 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using Player;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class LevelSwitcher : MonoBehaviour
 {
     [SerializeField] GameObject blackBackground;
-    bool shouldTransition;
+    [SerializeField] GameObject banner;
+    [SerializeField] AnimationCurve bannerMovement;
+    Image backgroundColor;
+
+    private void Awake()
+    {
+        backgroundColor = blackBackground.GetComponent<Image>();
+        
+    }
+
+    private void Start()
+    {
+        backgroundColor.CrossFadeAlphaWithCallBack(0, 1f, () => StartCoroutine(ShowBanner()));
+
+    }
+
+    IEnumerator ShowBanner()
+    {
+        float time = 0;
+        while (time < bannerMovement.keys[bannerMovement.keys.Length-1].time)
+        {
+            Debug.Log(time);
+            time += Time.deltaTime;
+
+            banner.transform.localPosition = new Vector3(0, bannerMovement.Evaluate(time), 0);
+
+            yield return null;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        AsyncOperation levelLoad;
-        if (name == "LevelEntrance")
-        {
-            levelLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex - 1);
-        }
-        else
-        {
-            levelLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
-        }
 
-        levelLoad.completed += (a) => GameObject.FindObjectOfType<PlayerController>().OnResumeGame();
+        backgroundColor.CrossFadeAlphaWithCallBack(1f, 1f, delegate
+        {
+            AsyncOperation levelLoad;
+            if (name == "LevelEntrance")
+
+            {
+                levelLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex - 1);
+            }
+            else
+            {
+                levelLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+
+            levelLoad.completed += (a) =>
+            {
+                FindObjectOfType<PlayerController>().OnResumeGame();
+            };
+        });
+
+        
+        
 
         // if (name == "LevelEntrance")
         // {
@@ -34,25 +75,10 @@ public class LevelSwitcher : MonoBehaviour
         
     }
 
-    // IEnumerator LoadLevelAsync()
-    // {
-    //
-    //
-    //     while (!levelLoad.isDone)
-    //     {
-    //         yield return null;
-    //     
-    //     }
-    //
-    //
-    //
-    // }
-
-    private void FixedUpdate()
-    {
-        if (shouldTransition)
-        {
-
-        }
-    }
+    //IEnumerator FadeToBlack()
+    //{
+    //    backgroundColor.CrossFadeAlpha(255f, 1f, true);
+    //    yield return new WaitForSeconds(1f);
+        
+    //}
 }

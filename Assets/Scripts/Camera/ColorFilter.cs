85e5aab6ebc8c0e6bd25fb4d3af0551cc7df0b5a
Simen Wiik <simenwiik@hotmail.com>
2021-11-26 10:12:07 +0100

    Adds color swaying

--- Assets/Scripts/Camera/ColorFilter.cs
+++ Assets/Scripts/Camera/ColorFilter.cs
@@ -14,6 +14,7 @@ public class ColorFilter : MonoBehaviour
     [SerializeField] AnimationCurve AllColors;
     private Bloom bloom = null;
     private ColorCurves colorCurves = null;
+    private TextureCurve activeCurve;
 
     // Start is called before the first frame update
     void Start()
@@ -26,19 +27,25 @@ public class ColorFilter : MonoBehaviour
 
     public void SetFilter(Filter filter)
     {
-        var newFilter = filter switch
+        activeCurve = filter switch
         {
             Filter.Gray => new TextureCurve(Gray, 0f, true, new Vector2(0f, 1f)),
             Filter.OnlyRed => new TextureCurve(OnlyRed, 0f, true, new Vector2(0f, 1f)),
             Filter.RedGreen => new TextureCurve(RedGreen, 0f, true, new Vector2(0f, 1f)),
             Filter.AllColors => new TextureCurve(AllColors, 0f, true, new Vector2(0f, 1f)),
         };
-        colorCurves.hueVsSat.Override(newFilter);
+        
+        colorCurves.hueVsSat.Override(activeCurve);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Keyframe red = new Keyframe(0, Mathf.Sin(Time.time));
+        Keyframe green = new Keyframe(0.5f, Mathf.Sin(Time.time + 0.5f));
+        Keyframe blue = new Keyframe(1f, Mathf.Sin(Time.time + 0.8f * 1.2f));
+        activeCurve.MoveKey(0, red);
+        activeCurve.MoveKey(1, green);
+        activeCurve.MoveKey(2, blue);
     }
 }

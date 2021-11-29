e5b1929334e1f1fc714c3d9bfe5b99002c4d9f9b
Ferskken <Andreashavnen@gmail.com>
2021-11-29 17:32:26 +0100

    World Chandes
    World Changes

--- Assets/Scripts/BossElevatorPlatform.cs
+++ Assets/Scripts/BossElevatorPlatform.cs
@@ -10,29 +10,54 @@ public class BossElevatorPlatform : MonoBehaviour
     [SerializeField] double yDestroy;
     [SerializeField] GameObject Music;
     [SerializeField] GameObject BossLoop;
+    public GameObject platform;
+    public bool timed;
+    public int timer = 3;
+    [SerializeField] public Vector2 resoawnPosition = new Vector2(0, 0);
+
+
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+     
         if (collision.gameObject.tag == "player")
         {
             moving = true;
             Music.SetActive(false);
             BossLoop.SetActive(true);
         }
+
+
+
     }
     private void Update()
     {
+        
+        
         if (moving)
         {
             transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), targetPosition, speed * Time.deltaTime);
+            if(timed)
+        {
+                Invoke("TimedFunction", timer);
+                Debug.Log(timer);
+            }
         }
-        if (transform.position.y < yDestroy )
+        
+        if (transform.position.y < yDestroy)
         {
             Destroy(this.gameObject);
         }
     }
+    private  void TimedFunction()
+    {
+        gameObject.transform.SetParent(null);
+        Destroy(gameObject);
+        Instantiate(platform, resoawnPosition, Quaternion.identity);
 
+        return;
+    }
 
 
 }
